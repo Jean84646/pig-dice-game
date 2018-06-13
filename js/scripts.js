5d04050355d3a4a1e00e2602eda6e2e8c995b09d
Anousone Kaseumsouk, Jean Jia and Lan Dam <anousone@>
2018-06-13 10:24:54 -0700

    Enter code to capture round score and dice roll, it works.

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,15 @@
 function Player(name, goal){
   this.name = name;
   this.goal = goal;
-  this.roundScore = 0;
+  // this.roundScore = 0;
   this.totalScore = 0;
   this.myTurn = false;
 }
 
 
-// get a random number between 0 and max-1
+// get a random number between 1 and max
 function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+  return Math.floor(Math.random() * Math.floor(max)+1);
 }
 
 
@@ -32,19 +32,29 @@ $(document).ready(function(){
 
     var playerOne = new Player("player1", goal);
     var playerTwo = new Player(p2, goal);
+    var turn = 0;
+    var diceRoll = 0;
+    var roundScore = 0;
 debugger;
     //randomly decide who to start
-    if(getRandomInt(2)){
+    if(getRandomInt(2)-1){
       playerOne.myTurn = true;
     }
 
     if(playerOne.myTurn){
       $('#user-turn').text(playerOne.name);
+      turn = 1;
     } else {
       $('#user-turn').text(playerTwo.name);
+      turn = 2;
     };
 
-
+    $('button#roll-dice').click(function(){
+      diceRoll = getRandomInt(6);
+      $('#dice-value').text(diceRoll.toString());
+      roundScore += diceRoll;
+      $('#current-score').text(roundScore.toString());
+    });
 
   });
 
