2d7d2506719fc5c556d9fab7a0b768cec4ffab76
Anousone Kaseumsouk, Jean Jia and Lan Dam <jean@>
2018-06-13 11:27:49 -0700

    Game working.

--- js/scripts.js
+++ js/scripts.js
@@ -24,12 +24,12 @@ $(document).ready(function(){
     var goal = $('input:radio[name=goal-option]:checked').val();
     var p2 = "";
     if(mode === "computer"){
-      p2 = "computer";
+      p2 = "Computer";
     } else {
-      p2 = "player2";
+      p2 = "Player TWO";
     };
 
-    var playerOne = new Player("player1");
+    var playerOne = new Player("Player ONE");
     var playerTwo = new Player(p2);
     var turn = 0;
     var diceRoll = 0;
@@ -39,29 +39,47 @@ $(document).ready(function(){
     turn = getRandomInt(2);
 
     if(turn === 1){
-      $('#user-turn').text(playerOne.name);
+      $('#user-turn').text(playerOne.name + " Turn");
     } else {
-      $('#user-turn').text(playerTwo.name);
+      $('#user-turn').text(playerTwo.name + " Turn");
     };
 
     $('button#roll-dice').click(function(){
       diceRoll = getRandomInt(6);
+      if(diceRoll === 1){
+        roundScore = 0;
+        if(turn === 1){
+          turn = 2;
+          $('#user-turn').text(playerTwo.name + " Turn");
+        } else {
+          turn = 1;
+          $('#user-turn').text(playerOne.name + " Turn");
+        };
+      } else {
+        roundScore += diceRoll;
+      }
       $('#dice-value').text(diceRoll.toString());
-      roundScore += diceRoll;
       $('#current-score').text(roundScore.toString());
+
     });
 
     $('button#end-turn').click(function(){
       if(turn === 1){
         playerOne.totalScore += roundScore;
         turn = 2;
-        $('#user-turn').text(playerTwo.name);
+        $('#user-turn').text(playerTwo.name + " Turn");
         $('#p1-total').text(playerOne.totalScore);
+        if(playerOne.totalScore >= goal){
+          $('#user-turn').text(playerOne.name + " WIN!!!");
+        };
       } else {
         playerTwo.totalScore += roundScore;
         turn = 1;
-        $('#user-turn').text(playerOne.name);
+        $('#user-turn').text(playerOne.name + " Turn");
         $('#p2-total').text(playerTwo.totalScore);
+        if(playerTwo.totalScore >= goal){
+          $('#user-turn').text(playerTwo.name + " WIN!!!");
+        };
       };
       roundScore = 0;
       $('#current-score').text(roundScore.toString());
